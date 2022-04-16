7fe6a92f8d2475340a1a8c2a82e5516750a3da62
CuongNguyen <Cuongn1912@gamil.com>
2022-04-16 13:38:11 +0700

    cuong

--- ticket_movie_fe/src/templates/ClientTemplate/Template/Header/Header.js
+++ ticket_movie_fe/src/templates/ClientTemplate/Template/Header/Header.js
@@ -4,9 +4,9 @@ import { history } from '../../../../App'
 import { useDispatch } from 'react-redux';
 import { Menu, Dropdown } from 'antd';
 import { DownOutlined } from '@ant-design/icons';
-
 import _ from 'lodash';
 import { SIGN_OUT } from '../../../../redux/Types/QuanLyNguoiDungType';
+import styles from './Header.module.css'
 export default function Header(props) {
     const userLogin = JSON.parse(sessionStorage.getItem("USER_LOGIN"));
     const dispatch = useDispatch();
@@ -21,11 +21,11 @@ export default function Header(props) {
         </Menu>
     );
     const handleLogin = () => {
-        return <Fragment>
-            {_.isEmpty(userLogin) ? <div className="items-center flex-shrink-0 hidden lg:flex">
-                <button onClick={() => { history.push('/signIn') }} className="font-monteCarlo self-center px-8 py-3 rounded hover:bg-violet-600 text-white">Đăng Nhập</button>
-                <button onClick={() => { history.push('/signUp') }} className="font-monteCarlo self-center px-8 py-3 font-semibold rounded hover:bg-violet-600 text-white">Đăng Kí</button>
-            </div> : <div className="items-center justify-center flex-shrink-0 hidden lg:flex text-white">
+        return <div className={`${styles.rs_btn}`} id='rs_btn'>
+            {_.isEmpty(userLogin) ? <div className="items-center  flex-shrink-0  lg:flex">
+                <button onClick={() => { history.push('/signIn') }} className={`${styles.rs_header} self-center px-6 py-3 rounded hover:bg-violet-600 text-white`}>Đăng Nhập</button>
+                <button onClick={() => { history.push('/signUp') }} className={`${styles.rs_header} self-center px-6 py-3 font-semibold rounded hover:bg-violet-600 text-white`}>Đăng Kí</button>
+            </div> : <div className="items-center  justify-center flex-shrink-0 hidden lg:flex text-white">
                 <Dropdown overlay={menu} trigger={['click']}>
                     <a className="ant-dropdown-link text-white" onClick={e => e.preventDefault()}>
                         <span className='mr-3'>{userLogin.userName}</span>    <DownOutlined />
@@ -34,32 +34,50 @@ export default function Header(props) {
 
             </div>
             }
-        </Fragment>
+        </div>
     }
     return (
-        <header className=" text-coolGray-800 fixed z-10 w-full bg-black bg-opacity-40 " >
-            <div className="container flex justify-between h-16 mx-auto">
-                <NavLink to='/home' aria-label="Back to homepage" className="flex items-center p-2" >
-                    <img style={{ width: '100%', height: '100%' }} src="https://movie.zalopay.vn/images/tix.svg" alt="logo" />
+        <nav className=" border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-800 text-coolGray-800 fixed z-10 w-full bg-black bg-opacity-40">
+            <div className="lg:container flex flex-wrap justify-between items-center mx-auto">
+                <NavLink to='/home' className="flex items-center">
+                    <img src="tix.png" className={`mr-3 h-6 sm:h-9 ${styles.logo}`} alt="Logo" />
                 </NavLink>
-                <ul className="items-stretch hidden space-x-3 lg:flex">
-                    <li className="flex">
-                        <NavLink to='/home' className="font-monteCarlo flex items-center px-4 -mb-1 border-b-2 border-transparent text-white " activeClassName='border-b-4 border-violet-600'>Trang Chủ</NavLink>
-                    </li>
-                    <li className="flex">
-                        <NavLink to='/GroupCinema' className="font-monteCarlo flex items-center text-white px-4 -mb-1 border-b-2 border-transparent" activeClassName='border-b-4 border-violet-600'>Cụm rạp</NavLink>
-                    </li>
+                <div>
+                    <button onClick={() => {
+                        const targetEl = document.getElementById('mobile_menu');
+                        const targetBtn = document.getElementById('rs_btn');
+                        // targetEl.style.display === 'block' ? targetEl.style.display = "none" : targetEl.style.display = 'block';
+                        if (targetEl.style.display === 'block') {
+                            targetEl.style.display = "none"
+                            targetBtn.style.display = 'none'
+                        } else {
+                            targetEl.style.display = 'block'
+                            targetBtn.style.display = 'none'
 
+                        }
+                    }} data-collapse-toggle="mobile_menu" type="button" className="inline-flex items-center p-2 ml-3 text-sm text-white rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2  dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="mobile_menu"
+                        targetEl='#mobile_menu' aria-expanded="false">
+                        <span className="sr-only">Open main menu</span>
+                        <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd" /></svg>
+                        <svg className="hidden w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd" /></svg>
+                    </button>
+                </div>
 
-                </ul>
+                <div className="hidden w-full md:block md:w-auto  lg:flex " id="mobile_menu">
+                    <ul className="md:flex justify-center items-center h-full mb-0 mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
+                        <li>
+                            <NavLink to='/home' className={`${styles.rs_header} block py-2 pr-4 pl-3  bg-blue-700 rounded md:bg-transparent text-white md:p-0 dark:text-white" aria-current="page" activeClassName='text-yellow-700`} >Trang Chủ</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to='/GroupCinema' className={`${styles.rs_header} block py-2 pr-4 pl-3 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 text-white md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700" activeClassName='text-blue-700`}>Cụm Rạp</NavLink>
+                        </li>
+                    </ul>
+
+                </div>
                 {handleLogin()}
-                <button className="p-4 lg:hidden">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="w-6 h-6 text-coolGray-800">
-                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
-                    </svg>
-                </button>
             </div>
-        </header>
+        </nav>
+
 
 
     )
